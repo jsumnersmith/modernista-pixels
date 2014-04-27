976916b175a880ae016f90aecb27907e39ecfe45
Joel Smith <jsumnersmith@gmail.com>
2014-04-26 21:13:22 -0400

    Scale it to screen size

--- site.js
+++ site.js
@@ -5,16 +5,25 @@ $(document).ready(function(){
 
   //  '#dc322f', '#d33682', '#6c71c4',
 
-  for (x=0; x<500; x++){
-    for (y=0; y<500; y++){
-      var random = Math.floor(Math.random() * ((colors.length - 1) - 0 + 1)) + 0;
-      style = style.concat((x*20)+'px '+(y*20)+'px '+colors[random]+", ");
+  function setStyle(){
+    var maxHeight = $(window).height() / 15;
+    var maxWidth = $(window).width() / 15;
+
+    console.log(maxHeight, maxWidth);
+    for (x=0; x<maxWidth; x++){
+      for (y=0; y<maxHeight; y++){
+        var random = Math.floor(Math.random() * ((colors.length - 1) - 0 + 1)) + 0;
+        style = style.concat((x*20)+'px '+(y*20)+'px '+colors[random]+", ");
+      }
     }
-  }
-  style = style.replace(/,\s*$/, "");
-  $single.css({
-    '-webkit-box-shadow': style,
-    '-moz-box-shadow': style,
-    'box-shadow': style,
-  })
+    style = style.replace(/,\s*$/, "");
+    $single.css({
+      '-webkit-box-shadow': style,
+      '-moz-box-shadow': style,
+      'box-shadow': style,
+    });
+  };
+
+  $(window).resize(setStyle());
+
 })
